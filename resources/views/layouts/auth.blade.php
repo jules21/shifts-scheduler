3c64f4255b766092bbbe1761e30d75e6e8ea08e6
jules21 <julesfabien96@gmail.com>
2019-08-25 13:02:59 +0200

    generate laravel auth scafold integrate frontend template

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('page-title') - {{ config('app.name') }}</title>
+    
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
+
+    @yield('header-scripts')
+</head>
+<body class="auth">
+
+    <div class="container">
+        @yield('content')
+    </div>
+    
+    <script src="{{ asset('assets/js/vendor.js') }}"></script>
+    <script src="{{ asset('assets/js/as/app.js') }}"></script>
+    <script src="{{ asset('assets/js/as/btn.js') }}"></script>
+    @yield('scripts')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,52 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.auth')
 
-        <title>Laravel</title>
+@section('page-title', trans('app.login'))
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@section('content')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<div class="col-md-8 col-lg-6 col-xl-5 mx-auto mt-4" id="login">
+    <div class="text-center">
+        <img src="{{ url('assets/img/app-logo.png') }}" alt="{{ config('app.name') }}" height="50" class="mb-5">
+    </div>
+    <div class="row">
+        <div class="col-sm-8">
+            
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <a href="{{ route('login') }}" class="text-center no-decoration">
+                        <div class="icon my-1">
+                            <i class="fas fa-user fa-2x"></i>
+                        </div>
+                        <p class="lead mb-0">Staff Login</p>
+                    </a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <a href="{{ route('login') }}" class="text-center no-decoration">
+                        <div class="icon my-1">
+                            <i class="fas fa-user fa-2x"></i>
+                        </div>
+                        <p class="lead mb-0">Employee Login</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="jumbotron jumbotron-fluid card" style="background-color:white">
+  <div class="container text-center">
+    <h1 class="display-4" style="color: #179970;">EMPLOYEE SHIFTS SCHEDULER SYSTEM</h1>
+
+  </div>
+</div>
+@stop
+
+@section('scripts')
+@stop
